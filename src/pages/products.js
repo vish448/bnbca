a9d10da060059809a53045690a53defeefc8e3ed
vishang soni <vishangsoni@gmail.com>
2021-04-23 00:08:36 -0400

    feat:age filter

--- src/pages/products.js
+++ src/pages/products.js
@@ -8,13 +8,23 @@ const ProductPage = ({ data }) =>  {
     const productsFilter = data.allMarkdownRemark.edges
     const csvItems = data.allProductsCsv.edges
     const [colorFilter,setColorFilter] = useState(null)
-    console.log('colorFilter',colorFilter)
-    if (colorFilter == null) {
+    const [ageFilter, setAgeFilter] = useState(null)
+    console.log('csvItems',csvItems)
+    console.log('products',productsFilter)
+    console.log('ageFilter',ageFilter)
+
+    const resetFilters = () => {
+        setColorFilter(null);
+        setAgeFilter(null);
+    }
+
+    if (colorFilter == null && ageFilter == null) {
         return ( 
             <ProductsList>
             <div className="products-list grid grid-cols-5 gap-4 p-8">
             <aside className="col-start-1">
                 FILTER BY:
+                    <h1 className="underline mb-2 text-lg mt-5">COLOR</h1>
                     {productsFilter.map(({node})=> {
                         const productColors = node.frontmatter.productColors
                         
@@ -27,8 +37,19 @@ const ProductPage = ({ data }) =>  {
                             </>
                         )
                     })}
-        
-                
+                    <h1 className="mt-5 underline text-lg mb-2">AGE</h1>
+                    {productsFilter.map(({node})=> {
+                        const age = node.frontmatter.age
+                        
+                        return(
+                            <>
+                                {age.map(age=> {
+                                    return(
+                                        <p className="cursor-pointer" onClick={()=>setAgeFilter(age)}>{age}</p>)
+                                })}
+                            </>
+                        )
+                    })}
             </aside>
             <main className="col-start-2 col-end-6">
                 <h1 className="text-4xl font-bold mb-8">Products</h1>
@@ -57,11 +78,13 @@ const ProductPage = ({ data }) =>  {
             </ProductsList>
             )
     }else{
+
         return ( 
             <ProductsList>
             <div className="products-list grid grid-cols-5 gap-4 p-8">
             <aside className="col-start-1">
                 FILTER BY:
+                    <h1 className="underline mb-2 text-lg mt-5">COLOR</h1>
                     {productsFilter.map(({node})=> {
                         const productColors = node.frontmatter.productColors
                         
@@ -69,22 +92,38 @@ const ProductPage = ({ data }) =>  {
                             <>
                                 {productColors.map(color=> {
                                     return(
-                                        <p className="cursor-pointer" onClick={()=>setColorFilter(color)}>{color}</p>
-                                        )
+                                        <p className="cursor-pointer" onClick={()=>setColorFilter(color)}>{color}</p>)
                                 })}
-                                <p className="cursor-pointer" onClick={()=>setColorFilter(null)}>Reset</p>
                             </>
                         )
                     })}
-        
-                
+                    <h1 className="mt-5 underline text-lg mb-2">AGE</h1>
+                    {productsFilter.map(({node})=> {
+                        const age = node.frontmatter.age
+                        
+                        return(
+                            <>
+                                {age.map(age=> {
+                                    return(
+                                        <p className="cursor-pointer" onClick={()=>setAgeFilter(age)}>{age}</p>)
+                                })}
+                            </>
+                        )
+                    })}
             </aside>
+           
             <main className="col-start-2 col-end-6">
-                <h1 className="text-4xl font-bold mb-8">Products</h1>
+                <h1 className="text-4xl font-bold mb-8">Products
+                    <span className="ml-10 text-sm">Filter: 
+                        
+                        <span className="bg-gray-100 p-1 pl-3 pr-3 rounded-xl" onClick={()=> (setColorFilter(null))}>{colorFilter}</span> 
+                        <span className="bg-gray-100 p-1 pl-3 pr-3 rounded-xl" onClick={()=> (setAgeFilter(null))}>{ageFilter}</span> <span className="bg-gray-100 p-1 pl-3 pr-3 rounded-xl cursor-pointer" onClick= {() => resetFilters()}>RESET</span>
+                    </span>
+                </h1>
                 <div className="product-list grid grid-cols-4 gap-4 mb-8">
                     {csvItems.map( ({node} ) => {
                         const productImageFluidCsv = getImage(node.productImage)
-                       
+                        
                         if (node.colors == colorFilter) {
                             return(
         
@@ -96,14 +135,31 @@ const ProductPage = ({ data }) =>  {
                                     <div className="product">
                                         <GatsbyImage image={productImageFluidCsv} alt="pimage" />
                                         <h1>{node.name}</h1>
-                                        <p>PRICE</p>
+                                        <p>{node.price}</p>
                                     </div>  
                                 </Link>
                             )
                         }
-
+                        if(node.sizes == ageFilter){
+                            return(
+        
+                                <Link
+                                        key={node.id}
+                                        to={node.fields.slug}
+                                        
+                                        >
+                                    <div className="product">
+                                        <GatsbyImage image={productImageFluidCsv} alt="pimage" />
+                                        <h1>{node.name}</h1>
+                                        <p>{node.price}</p>
+                                    </div>  
+                                </Link>
+                            )
+                        }
                         
                     })}
+                    
+                    
                 </div>
             </main>
             </div>
