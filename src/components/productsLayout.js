05cdbba92acda492ac2903c9b900fc0b06bb1875
vishang soni <vishangsoni@gmail.com>
2021-06-27 21:24:54 -0400

    removed the promo

--- src/components/productsLayout.js
+++ src/components/productsLayout.js
@@ -15,7 +15,7 @@ const productsLayout = ( {children} ) => {
             </Helmet>
             <div className="breadcrumb h-8 bg-gray-100 grid justify-items-center content-center">Products
             </div>
-            <div className="promo sm:m-8 grid justify-items-center content-center">
+            <div className="promo sm:m-8 grid justify-items-center content-center hidden">
             <StaticImage 
                 src="../images/promo.png" 
                 alt="promotion"
