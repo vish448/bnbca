cb0e2d4fc923af098a117f9c7a8b2d83c22fe98e
vishang soni <vishangsoni@gmail.com>
2021-07-14 23:14:36 -0400

    code update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,234 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { graphql } from 'gatsby'
-import { GatsbyImage, getImage, getImageData } from "gatsby-plugin-image"
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import React from 'react'
+import { Link} from 'gatsby'
+import { StaticImage, getImage, GatsbyImage } from "gatsby-plugin-image"
+import { MdLocalShipping, MdCollectionsBookmark } from "react-icons/md";
+import { AiFillInstagram} from "react-icons/ai";
+import { FaFacebookF, FaShippingFast } from "react-icons/fa";
 
-export default function ProductPage({ data }) {
-    let discountedRate
-    let finalPrice
-    let discount = data.productsCsv.discount
-    const productImageFluid = getImage(data.productsCsv.productImage)
-    const price = data.productsCsv.price
-    const productSizes = data.productsCsv.sizes.split(',')
-    const productColors = data.productsCsv.colors.split(',') 
-    const sku = data.productsCsv.sku.split(',')  
-    const productImage = data.productsCsv.productImage.childImageSharp.gatsbyImageData.images.fallback.src
-    if(discount){
-        discountedRate = price - ((price * discount)/100)
-        finalPrice = discountedRate.toFixed(2)
-    }
-
-    const [size, setSize] = useState('')
-    const [color, setColor] = useState('')
-    const [activeSize, setActiveSize] = useState(null)
-    const [activeColor, setActiveColor] = useState(null)
-    const [productData, setProductData] = useState(null)
-    const [isLoading, setIsLoading] = useState(true);
-
-    const toggleSize = (index) => {
-        setActiveSize((activeSize) => (activeSize === index ? null : index))
-    }
-
-    const toggleColor = (index) => {
-        setActiveColor((activeColor) => (activeColor === index ? null : index))
-    }
-
-    useEffect(() => {
-        getProductdata()
-    }, [])
-
-    async function getProductdata(){
-        setIsLoading(true);
-        const secret = `${process.env.GATSBY_SNIP_SECRET}`
-        const request = await fetch(`https://app.snipcart.com/api/products/${data.productsCsv.id}`, {
-            headers: {
-                'Authorization': `Basic ${btoa(secret)}`,
-                'Accept': 'application/json'
-            },
-        }).then((request => request.json()))
-          .then(data => setProductData(data))
-          .catch(err=>console.log(err))  
-          .finally(() => setIsLoading(false))
-    }
-    
-    let productSizeBuffer = ''
-    let productSizeOptions = ''
-    if (isLoading) return <div>Loading Data</div>;
-    return (
-        <>
-            <div className="breadcrumb h-8 bg-gray-100 grid justify-items-center content-center">
-                {data.productsCsv.slug}
-            </div>
-            <div className="grid grid-cols-1 justify-items-center sm:justify-items-start sm:grid-cols-2 gap-10 p-14 container mx-auto">
-                <div className="grid grid-cols-1">
-                        <div>
-                            <GatsbyImage image={productImageFluid} alt="pimage"/>
+const Footer = ({metaData})=> {
+    return(
+        
+            <footer className="mt-10">
+                <div className="footer-top h-40 md:grid grid-cols-3 hidden justify-items-center items-center bg-yellow-300">
+                    <div className="grid grid-cols-2">
+                        <MdLocalShipping className="text-8xl justify-self-center"/>
+                        <div className="self-center">
+                        <h1 className="text-2xl">Free Shipping</h1>
+                        <p>on orders above $79</p>
                         </div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <MdCollectionsBookmark className="text-8xl justify-self-center"/>
+                        <div className="self-center">
+                        <h1 className="text-2xl">Curated Collection</h1>
+                        <p>for every occasion</p>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <FaShippingFast className="text-8xl justify-self-center"/>
+                        <div className="self-center">
+                        <h1 className="text-2xl">We Ship</h1>
+                        <p> to North America</p>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div>
-                    <h1 className="product-name text-4xl pb-4">{data.productsCsv.name}</h1>
-                    <p className={`price pb-2 text-lg ${discount ? 'hidden' : 'block'}`}>$ {data.productsCsv.discountedPrice} <span className=" text-red-500 line-through">$ {data.productsCsv.price}</span></p>
-                    <p className={`price pb-2 text-lg ${discount ? 'hidden' : 'hidden'}`}>$ {data.productsCsv.price} </p>
-                    
-                    <p className={`discount mb-4 ${discount ? 'block' : 'hidden'}`}><span className="text-lg bg-green-600 text-white p-1">{discount}% OFF</span></p> 
-                    
-                    <p className="text-gray-500 pb-2">SKU: {sku[0]}</p>
-                    <div className="sizes mb-4 hidden">
-                        <p className="tracking-wider mb-2">Size</p>
-                        {productSizes.map((size, index) => {
-                            //productSizeBuffer += size + '|'
-                                return (
-                                    
-                                    <button className={`p-2 border-gray-200 border mr-2 mb-2 hover:bg-black hover:text-white cursor-pointer focus:border-black ${activeSize === index ? 'bg-black text-white' : null}`} role="button" tabIndex={0} 
-                                    onClick={() => {toggleSize(index); setSize(size)}}
-                                    onKeyDown={() => {toggleSize(index); setSize(size)}} key={index}>{size}</button>
-                                    
-                                )
-                            }
-                        )}
-                        
-                    </div> 
-                    <div className="colors mb-8 hidden">
-                        <p className="tracking-wider mb-2">Color</p>
-                        {productColors.map((color,index) => {
-                            return (
-                                <button className={`p-2 border-gray-200 border mr-2 ${color == 'black'? `hover:bg-black` :`hover:bg-${color}-500`} cursor-pointer outline-none ${activeColor === index ? `bg-gray-500 text-black` : null}`} role="button" tabIndex={0} 
-                                onClick={() => {toggleColor(index); setColor(color)}} onKeyDown = {()=> {toggleColor(index); setColor(size)}} key={index}>
-                                 {color}
-                                </button>
-                            )
-                            }
-                        )}
-                        
+                <div className="md:h-80 sm:h-auto bg-gray-100 grid grid-cols-1 md:grid-cols-5 p-8 lg:p-20 sm:text-center">
+                    <div className="text-center pt-0">
+                        <h1 className="text:1xl lg:text-2xl pb-2 font-bold">Useful Links</h1>
+                        <ul>
+                            <li className="p-3"><Link to="/faqs">FAQ's</Link></li>
+                            <li className="p-3"><Link to="/about-us">About Us</Link></li>
+                            <li className="p-3"><Link to="/contact-us">Contact Us</Link></li>
+                        </ul>
+                    </div>
 
-                    </div>  
-                   
-                    <div className="stock mb-4 ">
-                        <p className="tracking-wider mb-2">Size</p>
-                        {
-                            productData?(
-                                productData.variants.map((variant,index)=>{
-                                
-                                    if(variant.stock != 0){
-                                        return (
-                                            <>
-                                                <button className={`p-2 border-gray-200 border mr-2 mb-2 hover:bg-black hover:text-white cursor-pointer focus:border-black ${activeSize === index ? 'bg-black text-white' : null}`} role="button" tabIndex={0} 
-                                                onClick={() => {toggleSize(index); setSize(variant.variation[0].option)}}
-                                                onKeyDown={() => {toggleSize(index); setSize(variant.variation[0].option)}} key={index}>{variant.variation[0].option}</button>
-                                                <p className="hidden">{productSizeBuffer += variant.variation[0].option + '|'}</p>
-                                            </>    
-                                        )
-                                    }
-                                    else {
-                                        return(
-                                            <button className={`p-2 border-red-200 border mr-2 mb-2 ${variant.stock == 0 ?'bg-gray-100 text-gray-500 cursor-default outOfStock': null}`} disabled role="button" tabIndex={0}>{variant.variation[0].option}</button>
-                                        )
-                                    }
-                                })
-                            
-                            ):(
-                                productSizes.map((size, index) => {
-                                    //productSizeBuffer += size + '|'
-                                        return (
-                                            
-                                            <button className={`p-2 border-gray-200 border mr-2 mb-2 hover:bg-black hover:text-white cursor-pointer focus:border-black ${activeSize === index ? 'bg-black text-white' : null}`} role="button" tabIndex={0} 
-                                            onClick={() => {toggleSize(index); setSize(size)}}
-                                            onKeyDown={() => {toggleSize(index); setSize(size)}} key={index}>{size}</button>
-                                            
-                                        )
-                                    }
-                                )
-                            )
-                                
-                            }
+                    <div className="text-center">
+                        <h1 className="text:1xl lg:text-2xl pb-2 font-bold">Policies</h1>
+                        <ul>
+                            <li className="p-3"><Link to="/privacy-policy">Privacy Policy</Link></li>
+                            <li className="p-3"><Link to="/shipping-policy">Shipping Policy</Link></li>
+                            <li className="p-3"><Link to="/return-policy">Return/Exchange Policy</Link></li>
+                        </ul>
+                    </div>
+
+                    <div className="sm:col-span-1 lg:col-span-2">
+                        <StaticImage 
+                            src="../images/logo.png" 
+                            alt="Logo"
+                            placeholder="blurred"
+                            width={350}
+                        />
+                    </div>
+                    <div className="hidden">
                         
-                            
                     </div>
-                                 
-                    <p className="hidden">{productSizeOptions = productSizeBuffer.slice(0, -1)}</p>
                     
-                    <div className="product-details mb-4">
-                        <p className="tracking-wider mb-2"><b>Available Quantity:</b> {data.productsCsv.stock} availble for each size. Please enquire for additonal quanity at <a href="tel:9052696233">(905) 269-6233</a></p>
+                    <div className="text-center sm:col-span-2 lg:col-span-1">
+                        <h1 className="text:1xl lg:text-2xl pb-2 font-bold">Follow Us</h1>
+                        <ul>
+                            <li className="p-2 sm:p-0"><AiFillInstagram className="inline-block m-1"/>: <a target="_blank" href="https://www.instagram.com/bownbeecanada">bownbeecanada</a> <br/>
+                            </li>
+                            <li className="p-2 sm:p-0"><FaFacebookF className="inline-block m-1"/>: <a target="_blank" href="https://www.facebook.com/BownBeeCanada/">bownbeecanada</a></li>
+                            <p>We are one stop shop for all kids occasional dresses</p>
+
+                        </ul>
                     </div>
-                    <div className="product-details mb-4">
-                        <p className="tracking-wider mb-2 font-extrabold"><b>Product Details</b></p>
-                        <div>{data.productsCsv.productDetails} : {data.productsCsv.id}</div>
-                        <b>Specifications</b>
-                        <div>Brand Name - Bownbee</div>
-                        <div>Sleeves - {data.productsCsv.sleeves}</div>
-                        <div>Material - {data.productsCsv.materialName}</div>
-                        <div>Neck - {data.productsCsv.neckType}</div>
-                        <div>Fabric - {data.productsCsv.fabricType}</div>
-                        <div>WaistBand - {data.productsCsv.waistBand}</div>
-                        <div>Fit - {data.productsCsv.fit}</div><br/>
-                        <p>Washing Instructions</p>
-                        <div>{data.productsCsv.washCare}</div>
-                        <p>Items Included in the Package</p>
-                        <div>{data.productsCsv.packaging}</div>
-                    </div>
-                    <button className="btn bg-green-500 mt-4 snipcart-add-item p-4 rounded text-white"
-                            data-item-id={data.productsCsv.id}
-                            data-item-price={data.productsCsv.discountedPrice}
-                            data-item-url={`https://bownbee.ca/${data.productsCsv.productCategory}/${data.productsCsv.fields.slug}`}
-                            data-item-name={data.productsCsv.name}
-                            data-item-image={`https://bownbee.ca` + productImage}
-                            data-item-custom1-name="size" 
-                            data-item-custom1-value={size}
-                            data-item-custom1-options={productSizeOptions}
-                            data-item-custom1-required="true"
-                            data-item-max-quantity={data.productsCsv.stock}
-                            data-item-custom3-name="SKU"
-                            data-item-custom3-type="readonly"
-                            data-item-custom3-value={sku}
-                    >
-                            Add to cart
-                    </button>
                 </div>
-            </div>
-        </>
+                <div className="footer-bottom text-center sm:text-left sm:pl-20 pt-2 pb-2 text-sm text-gray-400">
+                &copy; {metaData}, {new Date().getFullYear()}
+                </div>
+            </footer>
+        
     )
-
 }
 
-export const pageQuery = graphql`
-  query ($id: String) {
-    productsCsv(id: {eq: $id}) {
-      id
-      name
-      productCategory
-      sizes
-      sku
-      stock
-      instock
-      description
-      colors
-      templateKey
-      price
-      discountedPrice
-      discount
-      fields {
-        slug
-      }
-      productImage {
-        id
-        childImageSharp {
-            fluid {
-                src
-            }
-            gatsbyImageData(width: 600, placeholder: BLURRED)
-        }
-      }
-      productDetails
-      sleeves
-      neckType
-      materialName
-      packaging
-      fabricType
-      fit
-      waistBand
-      washCare
-    }
-  } 
-`   
+export default Footer
