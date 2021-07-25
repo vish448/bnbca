163953e67e64426c289b47db62541d06878d9a65
vishang soni <vishangsoni@gmail.com>
2021-07-25 01:05:27 -0400

    updated product page

--- src/templates/products-page.js
+++ src/templates/products-page.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import { graphql } from 'gatsby'
-import { GatsbyImage, getImage, getImageData } from "gatsby-plugin-image"
+import { GatsbyImage, getImage} from "gatsby-plugin-image"
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
@@ -24,8 +24,7 @@ export default function ProductPage({ data }) {
     const [color, setColor] = useState('')
     const [activeSize, setActiveSize] = useState(null)
     const [activeColor, setActiveColor] = useState(null)
-    const [productData, setProductData] = useState(null)
-    const [isLoading, setIsLoading] = useState(true);
+ 
 
     const toggleSize = (index) => {
         setActiveSize((activeSize) => (activeSize === index ? null : index))
@@ -34,28 +33,13 @@ export default function ProductPage({ data }) {
     const toggleColor = (index) => {
         setActiveColor((activeColor) => (activeColor === index ? null : index))
     }
-    useEffect(() => {
-        getProductdata()
-    }, [])
 
-    async function getProductdata(){
-        setIsLoading(true);
-        const secret = `${process.env.GATSBY_SNIP_SECRET}`
-        const request = await fetch(`https://app.snipcart.com/api/products/${data.productsCsv.id}`, {
-            headers: {
-                'Authorization': `Basic ${btoa(secret)}`,
-                'Accept': 'application/json'
-            },
-        }).then((request => request.json()))
-          .then(data => setProductData(data))
-          .catch(err=>console.log(err))  
-          .finally(() => setIsLoading(false))
-    }
+
+
 
     let productSizeBuffer = ''
     let productSizeOptions = ''
-    if (isLoading) return <div>Loading Data</div>
-    console.log(productData)
+
     return (
         <>
             <div className="breadcrumb h-8 bg-gray-100 grid justify-items-center content-center">
@@ -76,7 +60,7 @@ export default function ProductPage({ data }) {
                     <p className={`discount mb-4 ${discount ? 'block' : 'hidden'}`}><span className="text-lg bg-green-600 text-white p-1">{discount}% OFF</span></p> 
                     
                     <p className="text-gray-500 pb-2">SKU: {sku[0]}</p>
-                    <div className="sizes mb-4 hidden">
+                    <div className="sizes mb-4">
                         <p className="tracking-wider mb-2">Size</p>
                         {productSizes.map((size, index) => {
                             productSizeBuffer += size + '|'
@@ -106,48 +90,6 @@ export default function ProductPage({ data }) {
                         {productSizeOptions = productSizeBuffer.slice(0, -1)}
 
                     </div> 
-                    <div className="stock mb-4 ">
-                        <p className="tracking-wider mb-2">Size</p>
-                        {
-                            productData?(
-                                productData.variants.map((variant,index)=>{
-
-                                    if(variant.stock != 0){
-                                        return (
-                                            <>
-                                                <button className={`p-2 border-gray-200 border mr-2 mb-2 hover:bg-black hover:text-white cursor-pointer focus:border-black ${activeSize === index ? 'bg-black text-white' : null}`} role="button" tabIndex={0} 
-                                                onClick={() => {toggleSize(index); setSize(variant.variation[0].option)}}
-                                                onKeyDown={() => {toggleSize(index); setSize(variant.variation[0].option)}} key={index}>{variant.variation[0].option}</button>
-                                                <p className="hidden">{productSizeBuffer += variant.variation[0].option + '|'}</p>
-                                            </>    
-                                        )
-                                    }
-                                    else {
-                                        return(
-                                            <button className={`p-2 border-red-200 border mr-2 mb-2 ${variant.stock == 0 ?'bg-gray-100 text-gray-500 cursor-default outOfStock': null}`} disabled role="button" tabIndex={0}>{variant.variation[0].option}</button>
-                                        )
-                                    }
-                                })
-
-                            ):(
-                                productSizes.map((size, index) => {
-                                    //productSizeBuffer += size + '|'
-                                        return (
-
-                                            <button className={`p-2 border-gray-200 border mr-2 mb-2 hover:bg-black hover:text-white cursor-pointer focus:border-black ${activeSize === index ? 'bg-black text-white' : null}`} role="button" tabIndex={0} 
-                                            onClick={() => {toggleSize(index); setSize(size)}}
-                                            onKeyDown={() => {toggleSize(index); setSize(size)}} key={index}>{size}</button>
-
-                                        )
-                                    }
-                                )
-                            )
-
-                            }
-
-
-                    </div>
-
                     <p className="hidden">{productSizeOptions = productSizeBuffer.slice(0, -1)}</p> 
                     <div className="product-details mb-4">
                         <p className="tracking-wider mb-2"><b>Available Quantity:</b> Please enquire for additonal quantity at <a href="tel:+61435593556">+61 435 593 556</a></p>
