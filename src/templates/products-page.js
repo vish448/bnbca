2f4bcf8b714d1d939aa3dc77f5d097817df94874
Vishang <vishangsoni@gmail.com>
2022-02-09 01:02:53 -0500

    updated product page

--- src/templates/products-page.js
+++ src/templates/products-page.js
@@ -11,7 +11,7 @@ export default function ProductPage({ data }) {
     let discount = data.productsCsv.discount
     const productImageFluid = getImage(data.productsCsv.productImage)
     //const productGallaryFluid = getImage(data.allFile.node)
-    //const productImgeSrc = data.productsCsv.productImage.childImageSharp.fluid.src
+    const productImgeSrc = data.productsCsv.productImage.childImageSharp.fluid.src
     const price = data.productsCsv.price
     const productSizes = data.productsCsv.sizes.split(',')
     const productColors = data.productsCsv.colors.split(',') 
